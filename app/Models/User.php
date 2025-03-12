c4f141cfab648196c0003c338a6f673a81335a15
zobirofkir <zobirofkir19@gmail.com>
2025-03-12 07:51:39 +0000

    add booted in User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 
 class User extends Authenticatable
 {
@@ -51,4 +54,16 @@ class User extends Authenticatable
     {
         return $this->hasMany(Message::class);
     }
+
+    protected static function booted()
+    {
+        Event::listen(Login::class, function ($event) {
+            $event->user->update(['is_active' => true]);
+        });
+
+        Event::listen(Logout::class, function ($event) {
+            $event->user->update(['is_active' => false]);
+        });
+    }
+
 }
