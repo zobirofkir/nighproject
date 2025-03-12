944da48e05fff89b90565f1ec2e8be22cf93c02e
zobirofkir <zobirofkir19@gmail.com>
2025-03-12 00:43:45 +0000

    create message migration and model and add relation sheep between messages and user

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
